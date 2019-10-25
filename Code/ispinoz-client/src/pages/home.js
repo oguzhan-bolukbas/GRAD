9d6ab531f2719ff4d4ca199795d7c2fe2182812f
= <=>
2019-10-25 11:19:55 +0300

    Cardview added.

--- Code/ispinoz-client/src/pages/home.js
+++ Code/ispinoz-client/src/pages/home.js
@@ -3,14 +3,16 @@ import '../App.css'
 
 // Components
 import Appbar from "../components/appbar";
-import {Stepper} from "@material-ui/core";
+import Stepper from "../components/stepper";
+import {Link} from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import {Typography} from "@material-ui/core";
+
 
 class Home extends Component{
   componentDidMount() {
-    document.body.style.background = "#333";
-    document.body.style.height = "100%";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundSize = "cover";
+    document.body.style.background = "#fff";
+
   }
   render() {
     return (
