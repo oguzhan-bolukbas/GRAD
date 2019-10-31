eb534c104e3a6a8b9e70ebe7059e05aec4a134ec
NurcihaneKoroglu <nurcihanekoroglu@marun.edu.tr>
2019-11-01 00:49:43 +0300

    New pages added. Terminology of the GA

---
+++ Code/ispinoz-client/src/pages/fitness.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Appbar from "../components/appbar";
+
+class Fitness extends Component {
+    render() {
+        return (
+            <div className="pixel">
+                <Appbar/>
+                <h1>Fitness</h1>
+                 </div>
+        );
+    }
+}
+
+export default Fitness;
