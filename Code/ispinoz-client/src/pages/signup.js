a476e0ce221ebdf8a1ce2ca6af2f463566f114a8
= <=>
2019-11-10 21:29:20 +0300

    Login and Signup pages translated to Turkish

--- Code/ispinoz-client/src/pages/signup.js
+++ Code/ispinoz-client/src/pages/signup.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import {Link} from "react-router-dom";
 import PropTypes from 'prop-types';
 import axios from "axios";
-
 // MUI Stuff
 import Grid from '@material-ui/core/Grid';
 import Typography from "@material-ui/core/Typography";
@@ -10,7 +9,6 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import withStyles from '@material-ui/core/styles/withStyles'
-
 // images
 import WebsiteIcon from '../images/European.png';
 import Background from "../images/DNA.png";
@@ -51,7 +49,7 @@ class signup extends Component {
       handle: this.state.handle
     };
     axios
-      .post('/signup', newUserData)
+      .post('/kayitol', newUserData)
       .then(res => {
         localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
         this.setState({
@@ -82,30 +80,30 @@ class signup extends Component {
         <Grid item sm>
           <img src={WebsiteIcon} width="99" height="104" alt="finch" className={classes.image}/>
           <Typography variant="h4" className={classes.pageTitle}>
-            Signup
+            Kayıt Ol
           </Typography>
           <form noValidate onSubmit={this.handleSubmit}>
-            <TextField id="email" name="email" type="email" label="Email" className={classes.textField}
+            <TextField id="email" name="email" type="email" label="E-posta" className={classes.textField}
                        helperText={errors.email} error={errors.email ? true : false} value={this.state.email}
                        onChange={this.handleChange} fullWidth/>
-            <TextField id="password" name="password" type="password" label="Password" className={classes.textField}
+            <TextField id="password" name="password" type="password" label="Şifre" className={classes.textField}
                        helperText={errors.password} error={errors.password ? true : false}
                        value={this.state.password} onChange={this.handleChange} fullWidth/>
-            <TextField id="confirmPassword" name="confirmPassword" type="password" label="Confirm Password"
+            <TextField id="confirmPassword" name="confirmPassword" type="password" label="Şifreyi Doğrula"
                        className={classes.textField}
                        helperText={errors.confirmPassword} error={errors.confirmPassword ? true : false}
                        value={this.state.confirmPassword}
                        onChange={this.handleChange} fullWidth/>
-            <TextField id="handle" name="handle" type="text" label="Username" className={classes.textField}
+            <TextField id="handle" name="handle" type="text" label="Kullanıcı Adı" className={classes.textField}
                        helperText={errors.handle} error={errors.handle ? true : false} value={this.state.handle}
                        onChange={this.handleChange} fullWidth/>
             {errors.general && (
               <Typography variant="body2" className={classes.customError}>{errors.general}</Typography>)}
             <Button type="submit" variant="contained" color="primary" className={classes.button}>
-              Signup
+              Kayıt
               {loading && (<CircularProgress size={30} color="inherit" className={classes.progress}/>)}
             </Button><br/>
-            <small>Already have an account? <Link to="/login">Login now!</Link></small>
+            <small>Zaten bir hesabınız var mı? <Link to="/girisyap">Şimdi Giriş Yap!</Link></small>
           </form>
         </Grid>
         <Grid item sm/>
